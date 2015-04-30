d60526649d8bbc6d02d1ce35a1c40a0123ab424e
kennygeiler <kennethlgeiler@gmail.com>
2015-04-30 18:08:57 -0400

    add answers model and migrations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,2 +1,6 @@
 class Answer < ActiveRecord::Base
+  belongs_to :question
+  belongs_to :user
+  has_many :responses, as: :responsible
+  validates :question, :user, presence: true
 end
--- db/migrate/20150430213027_create_answers.rb
+++ db/migrate/20150430213027_create_answers.rb
@@ -1,7 +1,9 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
-
+      t.text :content, null: false
+      t.references :user
+      t.references :question
       t.timestamps null: false
     end
   end
