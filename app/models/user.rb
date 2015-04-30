13b05c48db8e8770c2dd19fda9e0539e46981828
kennygeiler <kennethlgeiler@gmail.com>
2015-04-30 17:41:06 -0400

    add user model and migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  has_many :respones
+  has_many :questions
+  has_many :answers
+  has_many :votes
+  validates :name, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :reputation, presence: true
 end
