8c964c1e72d52da40f3ebf1d566379400268606e
kennygeiler <kennethlgeiler@gmail.com>
2015-04-30 18:02:59 -0400

    add response model and migration

--- db/migrate/20150430213233_create_responses.rb
+++ db/migrate/20150430213233_create_responses.rb
@@ -1,7 +1,9 @@
 class CreateResponses < ActiveRecord::Migration
   def change
     create_table :responses do |t|
-
+      t.text :content, null: false
+      t.references :user
+      t.references :imageable, polymorphic: true, index: true
       t.timestamps null: false
     end
   end
