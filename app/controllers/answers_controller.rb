db1a9ed6b173c1d5245847bc71f3b75751608b9d
kennygeiler <kennethlgeiler@gmail.com>
2015-05-01 17:56:54 -0400

    add edit answer functionality

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -20,6 +20,21 @@ class AnswersController < ApplicationController
     end
   end
 
+  def edit
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.find(params[:id])
+    if @answer.update_attributes(answer_params)
+      redirect_to question_path(@question)
+    else
+      render :edit
+    end
+  end
+
   def destroy
     question = Question.find(params[:question_id])
     answer = question.answers.find(params[:id])
