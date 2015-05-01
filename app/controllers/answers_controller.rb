209de6275728a0b54d855cf22ea15aed9177742a
Alexander Mihalopoulos <amihalop@gmail.com>
2015-05-01 14:59:37 -0400

    add answer#new and answer#create

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,25 @@
 class AnswersController < ApplicationController
+  def new
+    if current_user
+      @question = Question.find(params[:question_id])
+      @answer = @question.answers.new
+    else
+      flash[:error] = "You must be signed in"
+      redirect_to signin_path
+  end
+
+  def create
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.new(answer_params, user_id: current_user.id)
+
+    if @answer.save
+      redirect_to question_path
+    else
+      render :new
+    end
+  end
+
+  def answer_params
+    params.require(:answer).permit(:content)
+  end
 end
