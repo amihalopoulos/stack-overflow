770652ad4fc77b9286b7bd847080922d12b23016
kennygeiler <kennethlgeiler@gmail.com>
2015-05-01 16:54:47 -0400

    implemented edit response feature

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,9 +1,28 @@
 class ResponsesController < ApplicationController
 
-   def destroy
+  def edit
+    @response = Response.find(params[:id])
+  end
+
+  def update
+    @response = Response.find(params[:id])
+    if @response.update_attributes(response_params)
+      redirect_to question_path(@response.responsible_id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
     response = Response.find(params[:id])
     response.destroy
     redirect_to question_path(response.responsible_id)
+  end
+
+  private
+
+  def response_params
+    params.require(:response).permit(:content, :user_id)
   end
 
 end
