39cd1e90a2e55dc0882b5649a56b779953878476
Alexander Mihalopoulos <amihalop@gmail.com>
2015-04-30 20:25:19 -0400

    Add has_many votes to create_answers migration

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,7 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
   belongs_to :user
+  has_many :votes, as: :voteable
   has_many :responses, as: :responsible
   validates :question, :user, presence: true
 end
