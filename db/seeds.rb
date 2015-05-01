1dcb5e23e2a336986c713c79236736f36d8a1406
Alexander Mihalopoulos <amihalop@gmail.com>
2015-05-01 13:29:09 -0400

    Create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user = User.create(name: "mrpants", password: "yolo")
+
+5.times do
+  question = user.questions.create(title: "This is a question?", content: "This is a questions content")
+  2.times do
+    answer = question.answers.create(content: "this is an answer", user_id: user.id, question_id: question.id, user_id: user.id)
+    answer.responses.create(content: "this is a response", user_id: user.id)
+    question.responses.create(content: "this is a response", user_id: user.id)
+  end
+
+end
