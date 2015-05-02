d56eed94bd6c2bf2cc5ddfc42de7a8ad982cd18e
Charles Green <Charles.Green88@gmail.com>
2015-05-01 20:06:15 -0400

    add question_tag association and method to set tags

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,6 +3,20 @@ class Question < ActiveRecord::Base
   has_many :answers
   has_many :votes, as: :voteable
   has_many :tags
+  has_many :question_tags
+  has_many :tags, through: :question_tags
+
   has_many :responses, as: :responsible
   validates :title, :content, presence: true
+
+  def set_tags(tags)
+    self.tags = tags.split(", ").map do |name|
+      Tag.where(name: name.strip).first_or_create!
+    end
+  end
+
+  def all_tags
+    self.tags.map(&:name).join(", ")
+  end
+
 end
