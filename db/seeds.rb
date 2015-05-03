511a47c8baaf55e26b0c2e3e1ea554ce9c1f6882
Charles Green <Charles.Green88@gmail.com>
2015-05-03 19:15:33 -0400

    add styling to single question view,removed excess div and ul from response partial

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ user1 = User.create(name: "mrpants", password: "yolo")
 user2 = User.create(name: "Charles", password: "yolo")
 
 5.times do
-  question = user1.questions.create(title: Faker::Hacker.say_something_smart, content: Faker::Lorem.paragraph(2))
+  question = user1.questions.create(title: Faker::Hacker.say_something_smart, content: Faker::Lorem.paragraph(8))
   2.times do
     answer = question.answers.create(content: Faker::Hacker.say_something_smart, user_id: user2.id, question_id: question.id)
     answer.responses.create(content: Faker::Hacker.say_something_smart, user_id: user1.id)
