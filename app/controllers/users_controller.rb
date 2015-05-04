4c4a25a3ca6629ec22da414643586a8d206825f2
Alexander Mihalopoulos <amihalop@gmail.com>
2015-05-03 22:10:31 -0400

    add user.id to session to acknowledge signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save!
+      session[:user_id] = user.id
       redirect_to return_point, notice: "You are now signed in"
     else
       redirect_to new_user_path
