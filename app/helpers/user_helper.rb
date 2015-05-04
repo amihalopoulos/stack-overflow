3bb891065ae52f7a11a6f7aac855269786cebb95
Alexander Mihalopoulos <amihalop@gmail.com>
2015-05-03 21:18:21 -0400

    remove instance vars

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,18 +1,18 @@
 module UserHelper
 
   def rep_calculator(user_id)
-    @my_questions = Question.where(user_id: user_id)
-    @rep = 0
-    @my_questions.each do |q|
-      @rep += (q.votes.where(value: 1).count * 5)
-      @rep -= (q.votes.where(value: -1).count * 5)
+    my_questions = Question.where(user_id: user_id)
+    rep = 0
+    my_questions.each do |q|
+      rep += (q.votes.where(value: 1).count * 5)
+      rep -= (q.votes.where(value: -1).count * 5)
     end
-    @my_answers = Answer.where(user_id: user_id)
-    @my_answers.each do |a|
-      @rep += (a.votes.where(value: 1).count * 10)
-      @rep -= (a.votes.where(value: -1).count * 10)
+    my_answers = Answer.where(user_id: user_id)
+    my_answers.each do |a|
+      rep += (a.votes.where(value: 1).count * 10)
+      rep -= (a.votes.where(value: -1).count * 10)
     end
-    @rep
+    rep
   end
 
 
