668f61fbbf07c561ad4eb8895435e0d8de2d61ad
Alexander Mihalopoulos <amihalop@gmail.com>
2015-05-03 21:13:15 -0400

    allow for negative rep from downvotes

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -2,13 +2,15 @@ module UserHelper
 
   def rep_calculator(user_id)
     @my_questions = Question.where(user_id: user_id)
+    @rep = 0
     @my_questions.each do |q|
-      @rep = 0
-      @rep += (q.votes.count * 5)
+      @rep += (q.votes.where(value: 1).count * 5)
+      @rep -= (q.votes.where(value: -1).count * 5)
     end
     @my_answers = Answer.where(user_id: user_id)
     @my_answers.each do |a|
-      @rep += (a.votes.count * 10)
+      @rep += (a.votes.where(value: 1).count * 10)
+      @rep -= (a.votes.where(value: -1).count * 10)
     end
     @rep
   end
