39654bfc1a5dab4ca03b7dcae965bd233cfb284a
Alexander Mihalopoulos <amihalop@gmail.com>
2015-05-03 22:05:00 -0400

    add best_answer to question

--- db/migrate/20150430212632_create_questions.rb
+++ db/migrate/20150430212632_create_questions.rb
@@ -3,6 +3,7 @@ class CreateQuestions < ActiveRecord::Migration
     create_table :questions do |t|
       t.string :title, null: false
       t.text :content, null: false
+      t.integer :best_answer, default: nil
       t.integer :views, default: 0
       t.references :user, null: false
       t.timestamps null: false
